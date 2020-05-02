eac7c038af6a07d57df591757c5a7bfc1b6a0556
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-01 23:02:56 -0400

    Some button logic added

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/SignUp.scss";
 
 export default function SignUp() {
+  const [btnTxt, setBtn] = useState({
+    text: "Next",
+    active: true,
+  });
+
+  const [labelMsg, setMsg] = useState({
+    msg: "Sign Up For The IN-Dex Newsletter.",
+  });
+
+  const handleBtn = (e) => {
+    e.preventDefault();
+    setBtn({ text: "Sign Up" });
+    setMsg({ msg: "Almost done! Please Enter Your First and Last Name." });
+  };
+
   return (
     <section id="SignUp">
       <div className="signup-container">
         <h2>join the list</h2>
         <div className="form-container">
           <form>
-            <label for="init-input">Sign Up For The IN-Dex Newsletter.</label>
+            <label for="init-input">{labelMsg.msg}</label>
             <div className="form-group">
               <input
                 type="email"
                 placeholder="Enter Email Address"
                 className="input-box"
               />
-              <input type="submit" value="Next" className="submit-btn" />
+              <input
+                type="submit"
+                value={btnTxt.text}
+                onClick={handleBtn}
+                className="submit-btn"
+              />
             </div>
 
             <div className="privacy-container">
